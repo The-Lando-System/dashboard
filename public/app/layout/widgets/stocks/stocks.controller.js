d589c81af8a7f1cc94349b381253e81b90eec922
Matt <matt.voget@gmail.com>
2016-06-10 21:18:45 -0600

    spruced up the stocks widget

--- public/app/layout/widgets/stocks/stocks.controller.js
+++ public/app/layout/widgets/stocks/stocks.controller.js
@@ -9,20 +9,61 @@ function StocksWidgetController($http) {
   var stocksVm = this;
 
   stocksVm.loading = false;
+  stocksVm.applyChanges = applySettingsChange;
+  stocksVm.cancel = hideSettingsDialog;
+  stocksVm.openSettings = showSettingsDialog;
+  stocksVm.stockId = "NFLX";
 
-  var now = new Date();
-  var today = new Date(now.getTime() - now.getTimezoneOffset()*60*1000);
+  getStockInfo();
 
-  var stocksUrl = "https://www.quandl.com/api/v3/datasets/EOD/NFLX.json?api_key=s3pgWPGqafd8EmDtVyCo&start_date=" + today.toISOString().slice(0, 10);
 
-  $http.get(stocksUrl)
-  .success(function(data){
-  	stocksVm.stockData = data.dataset;
-  	stocksVm.latestPrice = stocksVm.stockData.data[0][stocksVm.stockData.data[0].length-2];
-  	stocksVm.stockCode = stocksVm.stockData.dataset_code;
-  })
-  .error(function(data){
+  function getStockInfo(){
+  	var now = new Date();
+  	var today = new Date(now.getTime() - now.getTimezoneOffset()*60*1000);
+  	var stocksUrl = "https://www.quandl.com/api/v3/datasets/EOD/" + stocksVm.stockId + ".json?api_key=s3pgWPGqafd8EmDtVyCo&start_date=" + today.toISOString().slice(0, 10);
+  	stocksVm.loading = true;
+  	$http.get(stocksUrl)
+    .success(function(data){
 
+  	  stocksVm.stockData = data.dataset;
+  	  stocksVm.stockName = data.dataset.name;
+  	  stocksVm.stockName = stocksVm.stockName.substring(0,(stocksVm.stockName.indexOf(stocksVm.stockId) - 1));
+
+  	  stocksVm.startPrice = stocksVm.stockData.data[0][1];
+  	  stocksVm.latestPrice = stocksVm.stockData.data[0][stocksVm.stockData.data[0].length-2];
+  	  stocksVm.increase = ((stocksVm.latestPrice - stocksVm.startPrice) > 0 ) ? true : false;
+  	  stocksVm.stockCode = stocksVm.stockData.dataset_code
+  	  stocksVm.loading = false;
+    })
+    .error(function(data){
+
+    });
+  };
+
+  function applySettingsChange(newStockId){
+  	stocksVm.stockId = newStockId;
+  	getStockInfo();
+  	hideSettingsDialog();
+  };
+
+  var settingsDialog;
+
+  function showSettingsDialog(){
+    if(!settingsDialog){
+      settingsDialog = document.querySelector('#settings-dialog-stocks');
+    }
+    settingsDialog.showModal();
+  };
+
+  function hideSettingsDialog(){
+    if(!settingsDialog){
+      settingsDialog = document.querySelector('#settings-dialog-stocks');
+    }
+    settingsDialog.close();
+  };
+
+  angular.element(document).ready(function () {
+  	componentHandler.upgradeAllRegistered();
   });
 
 };
