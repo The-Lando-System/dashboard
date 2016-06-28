ae661773e65dbe69ab5aeb2dc920477ff1623fb9
Matt <matt.voget@gmail.com>
2016-06-27 21:02:50 -0600

    moved login widget to navbar html and fixed bug in stocks

--- public/app/widgets/stocks/stock/stock.controller.js
+++ public/app/widgets/stocks/stock/stock.controller.js
@@ -18,42 +18,49 @@ function StockController($http, $scope) {
   });
 
 
-  function getStockInfo(requestDate){
+  function getStockInfo(){
 
-    if (!requestDate){
-  		requestDate = getShortDate(new Date());
+    var today = new Date();
+
+    var beginDate = getShortDate(new Date(today.getTime() - 24*60*60*1000));
+
+    // If it's Monday
+    if (today.getDay() === 1){
+
+      var beginDate = getShortDate(new Date(today.getTime() - 3*24*60*60*1000));
+
+    // If it's Sunday
+    } else if (today.getDay() === 0) {
+
+      var beginDate = getShortDate(new Date(today.getTime() - 2*24*60*60*1000));
+
+    // If it's Saturday
+    } else if (today.getDay() === 6) {
+
+      var beginDate = getShortDate(new Date(today.getTime() - 24*60*60*1000));
+
     }
-    var yesterdaysDate = getShortDate(new Date((new Date(requestDate)).getTime() - 24*60*60*1000));
-  	
-  	var stocksUrl = "https://www.quandl.com/api/v3/datasets/WIKI/" + stockVm.stockId + ".json?api_key=s3pgWPGqafd8EmDtVyCo&start_date=" + yesterdaysDate;
-  	
-  	$http.get(stocksUrl)
+
+    var stocksUrl = "https://www.quandl.com/api/v3/datasets/WIKI/" + stockVm.stockId + ".json?api_key=s3pgWPGqafd8EmDtVyCo&start_date=" + beginDate;
+    
+    $http.get(stocksUrl)
     .success(function(data){
 
-  	  if (data.dataset.newest_available_date !== requestDate){
-  	  	getStockInfo(data.dataset.newest_available_date);
-  	  } else {
+      stockVm.stockCode = data.dataset.dataset_code
+      stockVm.stockName = data.dataset.name.substring(0,(data.dataset.name.indexOf(stockVm.stockId) - 1));
+    
+      stockVm.todaysClose = data.dataset.data[0][4];
+      stockVm.yesterdaysClose = data.dataset.data[1][4];
+      stockVm.changeAmount = (stockVm.todaysClose - stockVm.yesterdaysClose).toFixed(2);
+      stockVm.increase = (stockVm.changeAmount > 0 ) ? true : false;
 
-        stockVm.stockCode = data.dataset.dataset_code
-        stockVm.stockName = data.dataset.name.substring(0,(data.dataset.name.indexOf(stockVm.stockId) - 1));
-      
-        stockVm.todaysClose = data.dataset.data[0][4];
-        stockVm.yesterdaysClose = data.dataset.data[1][4];
-        stockVm.changeAmount = (stockVm.todaysClose - stockVm.yesterdaysClose).toFixed(2);
-        stockVm.increase = (stockVm.changeAmount > 0 ) ? true : false;
-
-        
-  	  }
 
     })
     .error(function(data){
 
     });
-  };
 
-  function setStockData(data){
-
-  };
+  }
 
   function changeStockId(newStockId){
   	stockVm.stockId = newStockId;
@@ -62,7 +69,7 @@ function StockController($http, $scope) {
 
   angular.element(document).ready(function () {
   	componentHandler.upgradeAllRegistered();
-    getStockInfo();
+    getStockInfo('2016-06-24');
   });
 
   function getShortDate(date){
